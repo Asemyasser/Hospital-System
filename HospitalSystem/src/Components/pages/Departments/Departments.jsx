7240029a7d712be7c68f9be5d0c7b17966f27c9f
Anas Sayed mohamed <95660095+Anas-Sayed0@users.noreply.github.com>
2024-10-14 09:48:49 +0300

    add departments page

---
+++ HospitalSystem/src/Components/pages/Departments/Departments.jsx
@@ -0,0 +1,15 @@
+import DepartmentsSection from "./DepartmentsSection/DepartmentsSection";
+import Hero from "./Hero/Hero";
+
+import "./Departments.css";
+
+const Departments = () => {
+  return (
+    <>
+      <Hero />
+      <DepartmentsSection />
+    </>
+  );
+};
+
+export default Departments;
